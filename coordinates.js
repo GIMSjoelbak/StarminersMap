12734a7563639136fc0b1d426073e2e2289dccdb
GIMSjoelbak <125610769+GIMSjoelbak@users.noreply.github.com>
2023-02-17 09:52:55 +0100

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -31,4 +31,13 @@
     });
 
     L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);	 
+
+      const map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -2
+      });
 	  map.cursor.enable();
+      const bounds = [[0,0], [4881,8337]];
+      const image = L.imageOverlay('images/osrs_world_map_january11_2023.png', bounds).addTo(map);
+      
+      map.fitBounds(bounds);
