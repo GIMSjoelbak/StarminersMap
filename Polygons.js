22d9655182e854d6c79b2eafb3fb9105c85db3c7
GIMSjoelbak <125610769+GIMSjoelbak@users.noreply.github.com>
2023-02-27 18:53:56 +0100

    Update Polygons.js

--- Polygons.js
+++ Polygons.js
@@ -278,6 +278,11 @@ var MisthalinMarkers = L.layerGroup().addTo(map);
       calledLocation: d.calledLocation,
       shadow: false,
     };
+	  if (d.color === "red") {
+		  markerOptions.iconUrl = "https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers@master/img/marker-icon-red.png";
+	  } else if (d.color === "gold") {
+		  markerOptions.iconUrl = "https://cdn.jsdelivr.net/gh/pointhi/leaflet-color-markers@master/img/marker-icon-gold.png";
+	  }
     var marker = L.marker(d.latlng, markerOptions);
     if (d.color === "red") {
       marker.addTo(MisthalinMarkers);
