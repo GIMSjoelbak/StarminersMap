01ba16d7022650fab60c82ea04e39e8320185812
GIMSjoelbak <125610769+GIMSjoelbak@users.noreply.github.com>
2023-02-28 18:33:34 +0100

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -1,31 +1,4 @@
 
-L.CursorHandler = L.Handler.extend({
-
-        addHooks: function () {
-            this._popup = new L.Popup();
-            this._map.on('mouseover', this._open, this);
-	    this._map.on('mousemove', this._update, this);
-            this._map.on('mouseout', this._close, this);
-        },
-
-        removeHooks: function () {
-            this._map.off('mouseover', this._open, this);
-	    this._map.off('mousemove', this._update, this);
-            this._map.off('mouseout', this._close, this);
-        },
-
-        _open: function (e) {
-	    this._update(e);
-            this._popup.openOn(this._map);
-        },
-
-        _close: function () {
-            this._map.closePopup(this._popup);
-        },
-	
-
-    });
-
     L.Map.addInitHook('addHandler', 'cursor', L.CursorHandler);	 
 
       const map = L.map('map', {
