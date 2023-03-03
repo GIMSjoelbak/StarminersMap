e23df2a640d44bb23b53708c90a45989afb126b5
GIMSjoelbak <125610769+GIMSjoelbak@users.noreply.github.com>
2023-03-03 13:14:30 +0100

    Update Polygons.js

--- Polygons.js
+++ Polygons.js
@@ -274,9 +274,7 @@ Start of marker data
 */
 var logo = L.icon({
 	iconUrl: './images/logo.gif',
-	iconSize: [312, 263],
-	iconAnchor: [156, 131],
-	popupAnchor: [156, 131]
+	iconSize: L.point(312,263)
 });
     var logomarker = L.marker([1364,1268], { icon: logo }).addTo(map);
 logomarker.bindPopup('<a href="https://discord.gg/starminers">Click here to join the Discord');
